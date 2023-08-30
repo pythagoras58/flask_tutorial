b60d9401c009175211905bdd82719ce89ce0059d
pythagoras58 <solomondanso58@gmail.com>
2023-08-30 23:47:11 +0800

    Templates -2

--- _sample.py
+++ _sample.py
@@ -3,15 +3,30 @@ from flask import render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author':'Pythagorasweb',
+        'title': 'Love kills',
+        'content': 'Be careful',
+        'date_posted': 'May 12, 2023'
+    },
+    {
+        'author':'Pythagorasweb',
+        'title': 'Love is good',
+        'content': 'Follow your heart',
+        'date_posted': 'May 12, 2023'
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)
 
 
-@app.route("/save")
-def saveTheDate():
-    return "Babe"
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
