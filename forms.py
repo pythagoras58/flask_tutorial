64c1634fc81409c69c3e0367fc1f85b22b61cbdf
pythagoras58 <solomondanso58@gmail.com>
2023-09-01 14:15:20 +0800

    WTF Form - 2

--- forms.py
+++ forms.py
@@ -1,2 +1,7 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
 
+# form for registration
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=15)])
