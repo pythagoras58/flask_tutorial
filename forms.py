08c04bdb068472ba54aba9a2916fbd1c5d67eb11
pythagoras58 <solomondanso58@gmail.com>
2023-09-01 23:48:09 +0800

    Login Form

--- forms.py
+++ forms.py
@@ -1,7 +1,28 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 # form for registration
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=15)])
+
+    email = StringField('Email', validators=[DataRequired(), Email()])
+
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=5)])
+
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+
+    submit = SubmitField('Sign Up')
+
+
+
+# form for login
+class LoginForm(FlaskForm):
+
+    email = StringField('Email', validators=[DataRequired(), Email()])
+
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=5)])
+
+    remember = BooleanField('Remember Me')
+
+    submit = SubmitField('Sign In')
